4d949b12d0f60a1a51c781e4a6bf814289429b2e
gental <43147008+gental-py@users.noreply.github.com>
2023-06-07 14:30:19 +0200

    Add files via upload

---
+++ Backend/styles.py
@@ -0,0 +1,14 @@
+from colorama import Fore, init
+
+init()
+
+COLORS = {
+    "red": Fore.RED,
+    "green": Fore.GREEN,
+    "yellow": Fore.YELLOW,
+    "blue": Fore.BLUE,
+    "purple": Fore.MAGENTA,
+    "cyan": Fore.CYAN,
+    "white": Fore.WHITE,
+    "gray": Fore.LIGHTBLACK_EX,
+}
